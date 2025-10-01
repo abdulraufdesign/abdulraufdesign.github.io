2708371e16c495684fde9652d7b7fd54c50feb30
Abdul Rauf <177952790+abdulraufdesign@users.noreply.github.com>
2025-10-01 10:27:49 +0500

    Update media.js

--- media.js
+++ media.js
@@ -1,7 +1,7 @@
 var resumeCtas = document.querySelectorAll('.resume-cta');
 
 resumeCtas.forEach(resumeCta => {
-    resumeCta.setAttribute('href', 'https://drive.google.com/file/d/1jj03mWi4fCBvIZiiCnlGtFtZ-Oort-3T/view');
+    resumeCta.setAttribute('href', 'https://drive.google.com/file/d/1r2ziHPxDYcmetw0NE5tIKjHueL30dQwJ/view');
 });
 
 let projImgs = document.querySelectorAll('.proj-img');
